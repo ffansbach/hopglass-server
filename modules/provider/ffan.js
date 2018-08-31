f549f825798c3c9eb33d797be7d12d643ee9aa36
Wolfgang Rosenauer <wr@rosenauer.org>
2018-08-31 19:40:02 +0200

    fix a new typo

--- modules/provider/ffan.js
+++ modules/provider/ffan.js
@@ -72,7 +72,7 @@ module.exports = function(receiver, config) {
     var nodes = []
 
     async.forEachOf(data, function(n, k, finished) {
-      nodes.push(_.get(n, 'nodeinfo.hostname')+";"+_.get(n, 'nodeinfo.nodeid'))
+      nodes.push(_.get(n, 'nodeinfo.hostname')+";"+_.get(n, 'nodeinfo.node_id'))
       finished()
     }, function() {
       for (var i in nodes)
